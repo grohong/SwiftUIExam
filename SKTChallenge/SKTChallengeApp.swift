75aaf83d0b70d9eadb1f29e33169377ce5fb89ac
grohong <grohong76@gmail.com>
2024-03-24 14:52:45 +0900

    프로젝트 세팅

---
+++ SKTChallenge/SKTChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKTChallengeApp.swift
+//  SKTChallenge
+//
+//  Created by Hong Seong Ho on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SKTChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
